ef5f00b17851098a39b350bced31fad4a95dcb7f
Aziz <97131272+AzizMGV@users.noreply.github.com>
2022-03-20 18:56:11 +0300

    Create ContainsDuplicate_217.py

---
+++ ContainsDuplicate_217.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        array = set([])
+        
+        for num in nums:
+            if num in array:
+                return True
+            array.add(num)
+            
+        return False
